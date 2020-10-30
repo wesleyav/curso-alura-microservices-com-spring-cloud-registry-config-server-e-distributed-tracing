f8d7803550ddc28ce7c22cfc28202474776372d4
Wesley <wesley.contato@hotmail.com>
2020-10-30 07:46:16 -0300

    Implementando o serviço Fornecedor

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -2,11 +2,13 @@ package br.com.alura.microservice.loja.service;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 
+@Service
 public class CompraService {
 
 	public void realizaCompra(CompraDTO compra) {
